b9425149d12dda2d443ad97da105fa8500c386e2
JesusPastorini <juniorpastorini_@hotmail.com>
2023-07-06 05:16:52 -0300

    requisitos 11 ao 22

--- desafio11.sql
+++ desafio11.sql
@@ -1,1 +1,4 @@
-
+SELECT notes
+FROM purchase_orders
+WHERE notes LIKE 'Purchase generated based on Order #%'
+  AND SUBSTRING(notes, LENGTH(notes)-1, 2) BETWEEN '30' AND '39';
---
+++ desafio21.sql
@@ -0,0 +1,4 @@
+INSERT INTO order_details (order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
+SELECT 69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129
+UNION ALL
+SELECT 69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129;
